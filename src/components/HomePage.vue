f0e19cb3899d1ff9971b097592c48e0d68e43092
Adam <122129206+Amar-Adam-BENADJAOUD@users.noreply.github.com>
2024-12-11 10:27:32 -0800

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,37 +1,24 @@
 <!-- src/components/HomePage.vue -->
 <template>
-    <div>
-      <h1>Welcome to The Souk</h1>
-      <p>Please choose one of the following options:</p>
-      <div>
-        <button @click="goToLoginPage">Login</button>
-        <button @click="goToRegisterPage">New User</button>
-        <button @click="goToProductPage">Product Page</button>
-        <button @click="goToGroupPage">Group Page</button>
-        <button @click="goToSearchPage">Search Page</button>
-      </div>
+    <div id="home-content">
+      <h1>Welcome to The Souk !</h1>
+      <img src="./Logo.png" alt="Logo" id="home-logo">
+      <h3>The future of C2C marketplace</h3>
     </div>
   </template>
-  
-  <script>
-  export default {
-    methods: {
-      goToLoginPage() {
-        this.$router.push('/login');  // Navigate to the login page
-      },
-      goToRegisterPage() {
-        this.$router.push('/register');  // Navigate to the registration page
-      },
-      goToProductPage() {
-        this.$router.push('/product');  // Navigate to the product page
-      },
-      goToGroupPage() {
-        this.$router.push('/group');  // Navigate to the group page
-      },
-      goToSearchPage() {
-        this.$router.push('/search');  // Navigate to the search page
-      },
-    },
-  };
-  </script>
+
+<style>
+  #home-content{
+    margin-top: 10%;
+  }
+
+  #home-logo{
+    width: 25%;
+    height: auto;
+  }
+</style>
+
+
+
+
   
